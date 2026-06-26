5bcb76ea9882dd77c57b5ef995e9d33294e04100
LuisEd83 <eduluis784@gmail.com>
2026-06-25 22:06:29 -0300

    Update: Adicionado <na main.cpp> código que possibilita ativar ou não a criação de dados do disco de acreção e adicionado <no CMakeLists.txt> targets de execução.

--- main.cpp
+++ main.cpp
@@ -1,22 +1,27 @@
-#include "test_app.hpp"
 #include <cstdlib>
 #include <exception>
 #include <iostream>
 #include <string>
+#include "include/test_app.hpp"
 
 int main(int argc, char* argv[]) {
     try {
         if (argc > 1 && std::string(argv[1]) == "--export") {
-            std::string filename = (argc > 2) ? argv[2] : "output.png";
+            std::string filename = (argc > 2) ? argv[2] : "icons/output.png";
             uint32_t width = (argc > 3) ? std::stoi(argv[3]) : 1920;
             uint32_t height = (argc > 4) ? std::stoi(argv[4]) : 1080;
             
             VulkanTestApp app;
             app.exportFrame(filename, width, height);
+        }else if (argc > 1 && std::string(argv[1]) == "--disk") {
+            int n_rings = (argc > 2) ? std::stoi(argv[2]) : 200;
+            VulkanTestApp app;
+            app.exportDisk(n_rings);
         } else {
             VulkanTestApp app;
             app.run();
         }
+
     } catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
